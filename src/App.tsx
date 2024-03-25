cec9022c3bbc5744d65bfa708e290eaf3153ef76
cheezeman <pksam5046@naver.com>
2024-03-26 01:43:54 +0900

    Feat: prompt 명령을 통한 data GET

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import { callGPT } from "./api/gpt";
 
 function App() {
+  const [data, setData] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleClickAPICall = async () => {
-    await callGPT();
+    try {
+      setIsLoading(true);
+      const message = await callGPT({
+        prompt: `Open AI를 이용한 프로젝트를 시작했다. 프로젝트에 버그가 많이 나왔음. 구글에서 검색했지만 해결 안되었어.
+      역시 gpt를 통해서 해결했다. 근데 이렇게 해결하는게 개발실력에 도움 될까..?`,
+      });
+      setData(message);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <>
       <button onClick={handleClickAPICall}>GPT API Call</button>
+      <div>{data}</div>
+      <div>{isLoading ? "loading..." : "finished"}</div>
     </>
   );
 }
