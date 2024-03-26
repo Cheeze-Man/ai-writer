99c176444a7bf60d197ccaa581af14e2769d9421
cheezeman <pksam5046@naver.com>
2024-03-26 18:29:34 +0900

    Fix: 이미지를 던져주지 않는 버그 수정

--- src/api/gpt.ts
+++ src/api/gpt.ts
@@ -22,7 +22,7 @@ export const callGPT = async ({ prompt }: GPTtypes) => {
   4. [evaluates] : The written emotional [evaluates], using explore the unconscious based on the contents of the [emotional diary].
   6. [Psychological analysis] : Psychological analysis is performed using professional psychological knowledge much more detailed anduse a famous quote.
   7. [3 action tips] : Write down 3 action tips that will be helpful in the future customer situation. The three action tips must beconverted into JSON Array format.
-  8. [image] : Create an image by making the contents so far into one keyword.
+  8. [image] : Make the contents so far into one keyword, create an image, and make sure to show it in URL.
   
   
   Translate into Korean and Use the output in the following JSON format:
@@ -63,8 +63,11 @@ export const callGPT = async ({ prompt }: GPTtypes) => {
   });
 
   const responseData = await response.json();
+  console.log(responseData);
 
   const message = responseData.choices[0].message.content;
 
   return message;
 };
+
+// 8. [image] : Create an image by making the contents so far into one keyword.
